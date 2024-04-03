37342019942c2b3c01bff353b81adf63a603d7df
Shamil P P <56679389+shamilchennara84@users.noreply.github.com>
2024-04-03 14:09:33 +0530

    fix:chat profile pic

--- src/app/components/chat/conversation-list/conversation-list.component.ts
+++ src/app/components/chat/conversation-list/conversation-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IConversation, IConversationListItem } from '../../../core/models/interfaces/conversation';
 import { CommonModule } from '@angular/common';
 import { environment } from '../../../../environments/environment';
@@ -10,7 +10,15 @@ import { environment } from '../../../../environments/environment';
   templateUrl: './conversation-list.component.html',
   styleUrl: './conversation-list.component.css',
 })
-export class ConversationListComponent {
+export class ConversationListComponent implements OnInit {
   imgUrl: string = `${environment.imageUrl}`;
   @Input() conversation!: IConversationListItem;
+  userPlaceholderImageUrl: string = 'assets/placeholder/profile.png';
+  profileImg!: string;
+
+  ngOnInit(): void {
+    if(this.conversation)
+    this.profileImg = this.conversation.otherMemberProfilePic ? this.imgUrl + this.conversation.otherMemberProfilePic : this.userPlaceholderImageUrl;
+   
+  }
 }
